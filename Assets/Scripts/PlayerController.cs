0abd8ed666c2c4aa04de661ed3d7b8b5b7686060
Igor Altieres <igoraltieres@gmail.com>
2017-03-14 10:55:50 -0300

    Detectando colisoes e programando a morte

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,4 +34,15 @@ public class PlayerController : MonoBehaviour {
 
     }
 
+     void OnCollisionEnter(Collision outro)
+    {
+        if (outro.gameObject.tag == "obstaculo")
+        {
+            rb.AddForce(new Vector3(-50f, 20f, 0f), ForceMode.Impulse);
+            rb.detectCollisions = false;
+            anim.Play("morrendo");
+        }
+
+    }
+
 }
